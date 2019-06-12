4e5700bbbd61e6975fe4e4cfcfb3417eb032e5d2
Fidores <45700014+Fidores@users.noreply.github.com>
2019-06-12 18:57:30 +0200

    General CSS improvements.

--- src/app/services/quizzes/quizzes.service.ts
+++ src/app/services/quizzes/quizzes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Quiz } from 'src/app/models/quiz';
 
 @Injectable({
   providedIn: 'root'
@@ -11,44 +12,54 @@ export class QuizzesService {
     private readonly http: HttpClient
   ) { }
 
+  url = 'http://localhost:3000';
+
   getHomeSections(){
     return of([
       {
         title: 'Najnowsze',
         quizzes: [
-          { title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
-          { title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
+          { id: 1, title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 2, title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 3, title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
+          { id: 4, title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 5, title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 6, title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 7, title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
         ]
       },
       {
         title: 'Najpopularniejsze',
         quizzes: [
-          { title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
-          { title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
+          { id: 8, title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 9, title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 10,title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
+          { id: 11, title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 12, title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 13,title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 14, title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
         ]
       },
       {
         title: 'Najbardziej lubiane',
         quizzes: [
-          { title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
-          { title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
-          { title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
+          { id: 15, title: 'Jak dobrze znasz JavaScript 1', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 16, title: 'Jak dobrze znasz CSS3 1', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 17, title: 'Jak dobrze znasz HTML5 1', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' },
+          { id: 18, title: 'Jak dobrze znasz JavaScript 2', img: 'https://image.flaticon.com/icons/svg/919/919828.svg' },
+          { id: 19, title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 20,title: 'Jak dobrze znasz CSS3 2', img: 'https://image.flaticon.com/icons/svg/732/732190.svg' },
+          { id: 21,title: 'Jak dobrze znasz HTML5 2', img: 'https://image.flaticon.com/icons/svg/174/174854.svg' }
         ]
       }
     ]);
+  }
+
+  getQuizz(id: string): Observable<Quiz>{
+    return this.http.get<Quiz>(`${this.url}/api/quizzes/${id}`);
+  }
+
+  getAllQuizzes(): Observable<Quiz[]>{
+    return this.http.get<Quiz[]>(`${this.url}/api/quizzes`);
   }
 }
