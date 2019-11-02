2e50743f8124d917dc7ce68f936133e347d26f34
Fidores <45700014+Fidores@users.noreply.github.com>
2019-11-02 23:05:03 +0100

    Added more tests

--- src/app/user-quizzes/user-quizzes.component.spec.ts
+++ src/app/user-quizzes/user-quizzes.component.spec.ts
@@ -1,14 +1,46 @@
+import { UserService } from './../services/user/user.service';
+import { of } from 'rxjs';
+import { QuizzesService } from './../services/quizzes/quizzes.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TooltipDirective } from './../directives/tooltip/tooltip.directive';
+import { RouterModule } from '@angular/router';
+import { QuizComponent } from './../quiz/quiz.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserQuizzesComponent } from './user-quizzes.component';
+import { Quiz } from '../models/quiz';
 
 describe('UserQuizzesComponent', () => {
   let component: UserQuizzesComponent;
   let fixture: ComponentFixture<UserQuizzesComponent>;
+  let quizzesService;
+  let userService;
 
   beforeEach(async(() => {
+    quizzesService = class implements Partial<QuizzesService> {
+      getAllQuizzes = jasmine.createSpy('getAllQuizzes').and.returnValue(of([1, 2 ,3]));
+    }
+
+    userService = class implements Partial<UserService> {
+      getUser = jasmine.createSpy('getUser').and.returnValue(of({_id: '123456'}));
+    }
+
     TestBed.configureTestingModule({
-      declarations: [ UserQuizzesComponent ]
+      declarations: [ 
+        UserQuizzesComponent,
+        QuizComponent,
+        TooltipDirective
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+        FontAwesomeModule,
+        HttpClientModule
+      ],
+      providers: [
+        { provide: QuizzesService, useClass: quizzesService },
+        { provide: UserService, useClass: userService }
+      ]
     })
     .compileComponents();
   }));
@@ -17,6 +49,44 @@ describe('UserQuizzesComponent', () => {
     fixture = TestBed.createComponent(UserQuizzesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create component', () => {
+      expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+
+    it('should set quizzes property with data retuned from the server', () => {
+      component.ngOnInit();
+
+      expect(component.quizzes).toEqual([1, 2, 3] as unknown as Quiz[]);
+    });
+
+    it('it should call server to get user`s quizzes', () => {
+      const quizzes = fixture.debugElement.injector.get(QuizzesService);
+
+      expect(quizzes.getAllQuizzes).toHaveBeenCalledWith({ author: '123456' });
+    });
+
+    it('should call server to get user id', () => {
+      const user = fixture.debugElement.injector.get(UserService);
+
+      expect(user.getUser).toHaveBeenCalled();
+    })
+
+  });
+
+  describe('onQuizDelete', () => {
+
+    it('should remove quiz from quizzes array', () => {
+      component.quizzes = [1, 2, 3, 4, 5] as unknown as Quiz[];
+
+      component.onQuizDelete(component.quizzes[0]);
+
+      expect(component.quizzes).toEqual([2, 3, 4, 5] as unknown as Quiz[]);
+    });
+
   });
 
 });
--- src/app/view/image-input/image-input.component.spec.ts
+++ src/app/view/image-input/image-input.component.spec.ts
@@ -1,6 +1,8 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageInputComponent } from './image-input.component';
+import { By } from '@angular/platform-browser';
 
 describe('ImageInputComponent', () => {
   let component: ImageInputComponent;
@@ -8,7 +10,12 @@ describe('ImageInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageInputComponent ]
+      declarations: [ 
+        ImageInputComponent
+      ],
+      imports: [
+        FontAwesomeModule
+      ]
     })
     .compileComponents();
   }));
@@ -19,5 +26,131 @@ describe('ImageInputComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+
+    it('should add event listener to file reader', () => {
+      const spy = spyOn(component.reader, 'addEventListener').and.callFake((event$, callback) => null);
+
+      component.ngOnInit();
+
+      expect(component.reader.addEventListener).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('writeValue', () => {
+
+    it('should set src attribute of the image element', () => {
+      const de = fixture.debugElement.query(By.css('.image-input__image'));
+      const el: HTMLImageElement = de.nativeElement;
+
+      component.writeValue('data:image/png;base64,testvalue');
+
+      expect(el.src).toBe('data:image/png;base64,testvalue');
+    });
+
+  });
+
+  describe('registerOnChange', () => {
+
+    it('should set change property with function received in the argument', () => {
+      const change = () => null;
+      
+      component.registerOnChange(change);
+
+      expect(component.change).toEqual(change);
+    });
+
+  });
+
+  describe('registerOnTouched', () => {
+
+    it('should set touched property with function received in the argument', () => {
+      const touched = () => null;
+      
+      component.registerOnTouched(touched);
+
+      expect(component.touched).toEqual(touched);
+    });
+
+  });
+
+  describe('onTouched', () => {
+
+    it('should call touched method', () => {
+      component.touched = () => null;
+      const spy = spyOn(component, 'touched').and.returnValue(null);
+
+      component.onTouched();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('removeImage', () => {
+
+    it('should set src of the image element to empty string', () => {
+      const de = fixture.debugElement.query(By.css('.image-input__image'));
+      const el: HTMLImageElement = de.nativeElement;
+
+      el.setAttribute('src', 'data:image/png;base64,testvalue');
+      component.removeImage();
+      
+      expect(el.src).not.toContain('data:image/png;base64,testvalue');
+    });
+
+    it('should remove image input value', () => {
+      const de = fixture.debugElement.query(By.css('.image-input__input'));
+      const el: HTMLInputElement = de.nativeElement;
+      const spy = spyOnProperty(el, 'value', 'set');
+
+      component.removeImage();
+
+      expect(spy).toHaveBeenCalledWith('');
+    });
+
+  });
+
+  describe('onChange', () => {
+
+    beforeEach(() => {
+      component.change = () => null;
+      component.touched = () => null;
+    })
+
+    it('should call change method', async () => {
+      const spy = spyOn(component, 'onChange').and.returnValue(null);
+
+      await component.onChange(new Event('change'));
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should call touched method', async () => {
+      const files = { files: [ new Blob(['']) ] } as unknown as HTMLInputElement;
+      component.touched = jasmine.createSpy('touched').and.returnValue(null);
+      component.imgInput.nativeElement = files;
+
+      await component.onChange(new Event('change'));
+
+      expect(component.touched).toHaveBeenCalled();
+    });
+
+    it('should call readAsDataURL method of the file reader', async () => {
+      const files = { files: [ new Blob(['']) ] } as unknown as HTMLInputElement;
+      const spy = spyOn(component.reader, 'readAsDataURL').and.returnValue('testvalue');
+      component.imgInput.nativeElement = files;
+
+      await component.onChange(new Event('change'));
+
+      expect(spy).toHaveBeenCalledWith(files.files[0]);
+    });
+
+  });
 
 });
